6fd906acd5e8d289984e2ebff38262421bcc7250
AntonioM788 <145494163+AntonioM788@users.noreply.github.com>
2023-12-06 12:04:44 +0100

    Create create.php

---
+++ create.php
@@ -0,0 +1,62 @@
+<?php
+include 'config.php';
+
+$message = '';
+
+if ($_SERVER["REQUEST_METHOD"] == "POST") {
+    $nombre = $_POST['nombre'];
+    $autor = $_POST['autor'];
+    $descripcion = $_POST['descripcion'];
+    $editorial = $_POST['editorial'];
+    $precio = $_POST['precio'];
+    $stock = $_POST['stock'];
+
+    try {
+        $sql = "INSERT INTO libro (nombre, autor, descripcion, editorial, precio, stock) VALUES (:nombre,:autor, :descripcion, editorial:, :precio, :stock)";
+        $stmt = $pdo->prepare($sql);
+        $stmt->execute(['nombre' => $nombre, 'autor' => $autor, 'descripcion' => $descripcion, 'editorial' => $editorial, 'precio' => $precio, 'stock' => $stock]);
+
+        $message = 'libro añadido con éxito!';
+    } catch (PDOException $e) {
+        $message = 'Error al añadir el libro: ' . $e->getMessage();
+    }
+}
+?>
+<!DOCTYPE html>
+<html lang="es">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Añadir Libro</title>
+</head>
+<body>
+<h2>Añadir nuevo LIbro</h2>
+
+<?php if (!empty($message)): ?>
+    <p><?= $message ?></p>
+<?php endif; ?>
+
+<form action="create.php" method="post">
+    <label for="nombre">Nombre:</label>
+    <input type="text" name="nombre" id="nombre" required>
+    <br>
+    <label for="autor">Autor:</label>
+    <input type="text" name="autor" id="autor" required>
+    <br>
+    <label for="descripcion">Descripción:</label>
+    <textarea name="descripcion" id="descripcion"></textarea>
+    <br>
+    <label for="editorial">Editorial:</label>
+    <input type="text" name="editorial" id="editorial" required>
+    <br>
+    <label for="precio">Precio:</label>
+    <input type="text" name="precio" id="precio" required>
+    <br>
+    <label for="stock">Stock:</label>
+    <input type="number" name="stock" id="stock" required>
+    <br>
+    <input type="submit" value="Añadir Libro">
+</form>
+
+</body>
+</html>
