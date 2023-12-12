d63736039d2b62febc378fa06a6486f2be15c135
AntonioM788 <145494163+AntonioM788@users.noreply.github.com>
2023-12-12 20:53:08 +0100

    Update edit.php

--- edit.php
+++ edit.php
@@ -3,11 +3,11 @@ include 'config.php';
 
 // Comprobando si se ha enviado el formulario
 if ($_SERVER["REQUEST_METHOD"] == "POST") {
-    $nombre = $_POST['nombre'];
+    $nombre = $_POST['titulo'];
     $precio = $_POST['precio'];
     $id = $_POST['id'];
 
-    $stmt = $pdo->prepare("UPDATE libros SET nombre = ?, precio = ? WHERE id = ?");
+    $stmt = $pdo->prepare("UPDATE libros SET titulo = ?, precio = ? WHERE id = ?");
     $stmt->execute([$nombre, $precio, $id]);
 
     header('Location: index.php');
@@ -24,7 +24,7 @@ $jabon = $stmt->fetch();
 
 <form action="edit.php" method="post">
     <input type="hidden" name="id" value="<?php echo $libro['id']; ?>">
-    Nombre: <input type="text" name="nombre" value="<?php echo $libro['nombre']; ?>"><br>
+    Nombre: <input type="text" name="titulo" value="<?php echo $libro['titulo']; ?>"><br>
     Precio: $<input type="text" name="precio" value="<?php echo $libro['precio']; ?>"><br>
     <input type="submit" value="Guardar Cambios">
 </form>
